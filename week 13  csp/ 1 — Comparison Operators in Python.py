af3039c605c880a193b18de68966b370d2bc68dd
yogurt3841 <allyc525@gmail.com>
2025-11-12 15:07:56 +0000

    operators

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -12,24 +12,69 @@
 
 a = 3
 b = 4
+print(a) #output 3
+print(b) #output 4
+print(a==b) #output False
 
-print(a == b)   # False
-print(a != b)   # True
-print(a > b)    # False
-print(a < b)    # True
-print(a >= b)   # False
-print(a <= b)   # True
+print(a == b)   #Checks for equality # False
+print(a != b)   # checks if is not equal # True
+print(a > b)    # checks for greater than  # False
+print(a < b)    # checks for less than # True
+print(a >= b)   # checks for greater than or equal to # False
+print(a <= b)   # Checks for less than or equal; to # True
 
 
 #predict the output of the following comparisons:
-10 > 5
-7 == 2 * 3 + 1
-8 != 8
-4 <= 2 + 2
+10 > 5 #output true
+7 == 2 * 3 + 1 #output true
+8 != 8 #output False
+4 <= 2 + 2 #output true
 
 # Write 3 examples that result in True and 3 that result in False.
+7>6 #output true
+8 == 2+2 * 2 #ouput true
+16 <= 8* 2 #output true
+
+6 != 6 #output false
+14 == 7 +5 #output false
+5<2 #output false
 
 # Create a simple grade-checking condition:
 
 # practice problem :
 # where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+
+# asking student score
+score= int(input("what is your score?"))
+# make this program for all grading spectrums
+# if the score is between 90 -100.. you got an A
+# if the score is between 80-89-- you got an B
+# if the score is between 70-79 --- you got an C
+# if between 61-69 ... you got a D
+# if else you failed
+
+# if score >=60:
+ #  print("you passed the test")
+#   else:
+ #  print("you didn't pass")
+
+# ask for password
+  # password=input("what is your password")
+
+# & is and operator
+# | is
+# ~ is not operator
+
+
+
+if score >=90 and score <=100:
+    print("you got an A and passed!")
+elif score >=80 and score <=89:
+     print("you got an B and passed!")
+elif score >=70 and score <=79:
+     print("you got an C")
+elif score >=60 and score <=69:
+     print(" you got an D")
+else:
+     print('you failed, come for ACLAB') 
+
